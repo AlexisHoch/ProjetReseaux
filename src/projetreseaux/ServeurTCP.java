2a786a9a5c78d3148087d5963c0103d566327b19
AlexisHoch <37694448+AlexisHoch@users.noreply.github.com>
2018-12-10 19:52:44 +0100

    -*last

--- src/projetreseaux/ServeurTCP.java
+++ src/projetreseaux/ServeurTCP.java
@@ -6,25 +6,14 @@
 package projetreseaux;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import static java.lang.System.out;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 
 /**
  *
@@ -32,87 +21,44 @@ import javax.crypto.NoSuchPaddingException;
  */
 public class ServeurTCP {
     final static int port = 4444;
-    final static ResoNom rn = new ResoNom();
     final static ChiffrementAES chif = new ChiffrementAES();
+    final static String KEY_FILE = "cle.txt";
+    static boolean reading = false;
     
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, Exception{
      InetAddress address;
      address = InetAddress.getLocalHost();
      String[] infos = address.toString().split("/");
-     Scanner sc = new Scanner(System.in);
-        try {
+     
+     ChiffrementAES.key = ChiffrementAES.generateKey();
+     ChiffrementAES.exportKey(ChiffrementAES.key, KEY_FILE);
+     
 
             
             ServerSocket socketserveur = new ServerSocket(port);
             System.out.println("lancement du serveur");
             System.out.println("Nom du serveur : "+infos[0]);
             System.out.println("Adresse du serveur : "+infos[1]);
-            
-            
-            
-            
             Socket socketClient = socketserveur.accept();
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(socketClient.getInputStream()));
+            DataInputStream In = new DataInputStream(socketClient.getInputStream());
+            DataOutputStream Out = new DataOutputStream(socketClient.getOutputStream());
+            BufferedReader Input = new BufferedReader(new InputStreamReader(System.in));
             
-            PrintWriter pw = new PrintWriter(
-                    new BufferedWriter(
-                            new OutputStreamWriter(socketClient.getOutputStream())), true);
             
             System.out.println("Commexion avec : "+socketClient.getInetAddress());
             
-            
-            Thread envoi = new Thread(new Runnable() {
-                String msg;
-                @Override
-                public void run() {
-                    while(true){
-                        msg=sc.nextLine();
-                        try {
-                            byte[] msgchiffré = chif.chiffrerMsg(msg);
-                        } catch (InvalidKeyException ex) {
-                            Logger.getLogger(ServeurTCP.class.getName()).log(Level.SEVERE, null, ex);
-                        } catch (NoSuchAlgorithmException ex) {
-                            Logger.getLogger(ServeurTCP.class.getName()).log(Level.SEVERE, null, ex);
-                        } catch (NoSuchPaddingException ex) {
-                            Logger.getLogger(ServeurTCP.class.getName()).log(Level.SEVERE, null, ex);
-                        } catch (IllegalBlockSizeException ex) {
-                            Logger.getLogger(ServeurTCP.class.getName()).log(Level.SEVERE, null, ex);
-                        } catch (BadPaddingException ex) {
-                            Logger.getLogger(ServeurTCP.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        pw.println(msg);
-                        pw.flush();
-                    }
+            String message= "";
+            while(!message.equals("bye")){
+                if(!reading){
+                    ChiffrementAES.dechiffrerMsg(In);
                 }
-            });
-            envoi.start();
+                else{
+                    message = Input.readLine();
  
-            
-            
-            Thread recevoir = new Thread(new Runnable() {
-                String msg;
-                @Override
-                public void run() {
-                    try{
-                        msg = in.readLine();
-                        
-                        while(!msg.equals("bye")){
-                            System.out.println("Client : "+msg);
-                            msg=in.readLine();
-                        }
-                        System.out.println("Client déconnecté");
-                        out.close();
-                        socketClient.close();
-                        socketserveur.close();
-                        } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
+                   byte[] tab = ChiffrementAES.chiffrerMsg(message);
+                   Out.writeInt(tab.length);
+                   Out.write(tab);
                 }
-            });
-            recevoir.start();
-        }catch (IOException e ){
-            e.printStackTrace();
-        }
+            }
     }
 }
